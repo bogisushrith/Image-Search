b146c2d522cd2d84579dfd7239b04da4c43aebd0
sushrith <bogisushrith@gmail.com>
2023-03-24 14:40:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Image-Search
+
+## Configuration
+
+To configure use a different Unsplash API key, update the ApiKey constant in App.vue to your API key: const apiKey = 'Your-Unsplash-API-Key';
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="color">
+    <h2>Image Search</h2>
+    <input type="text" v-model="SearchTerm" placeholder="Enter search term"><br>
+    <button @click="searchImages">Search</button>
+    <div v-if="ImageUrl">
+      <img :src="ImageUrl" alt="Image result">
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      ApiKey: 'Your-Unsplash-API-Key',
+      SearchTerm: '',
+      ImageUrl: '',
+    }
+  },
+  methods: {
+    async searchImages() {
+      const response = await fetch(`https://api.unsplash.com/photos/random/?client_id=${this.ApiKey}&query=${this.SearchTerm}`);
+      const data = await response.json();
+      this.ImageUrl = data.urls.regular;
+    }
+  }
+}
+</script>
+
+
+<style>
+.color {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 2rem;
+  background-color: aqua;
+}
+</style>
